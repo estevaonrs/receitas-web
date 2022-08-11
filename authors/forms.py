f099a50ec04caee1d687abf5a500b52b6813ce79
Estevao Ferreira Pequeno <tevonrs@gmail.com>
2022-08-11 01:47:07 -0300

    tests dos forms

--- authors/forms.py
+++ authors/forms.py
@@ -6,8 +6,8 @@ from django.core.exceptions import ValidationError
 
 
 def add_attr(field, attr_name, attr_new_val):
-    existing_Attr = field.widget.attrs.get(attr_name, '')
-    field.widget.attrs[attr_name] = f'{existing_Attr} {attr_new_val}'.strip
+    existing = field.widget.attrs.get(attr_name, '')
+    field.widget.attrs[attr_name] = f'{existing} {attr_new_val}'.strip()
 
 
 def add_placeholder(field, placeholder_val):
@@ -15,15 +15,14 @@ def add_placeholder(field, placeholder_val):
 
 
 def strong_password(password):
-    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}')
-
+    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
     if not regex.match(password):
         raise ValidationError((
             'Password must have at least one uppercase letter, '
             'one lowercase letter and one number. The length should be '
-            'at least 8 characters'
+            'at least 8 characters.'
         ),
-            code='Invalid'
+            code='invalid'
         )
 
 
@@ -35,12 +34,12 @@ class RegisterForm(forms.ModelForm):
         add_placeholder(self.fields['first_name'], 'Ex.: John')
         add_placeholder(self.fields['last_name'], 'Ex.: Doe')
         add_attr(self.fields['username'], 'css', 'a-css-class')
+        add_placeholder(self.fields['password'], 'Type your password')
+        add_placeholder(self.fields['password2'], 'Repeat your password')
 
     password = forms.CharField(
         required=True,
-        widget=forms.PasswordInput(attrs={
-            'placeholder': 'Your password'
-        }),
+        widget=forms.PasswordInput(),
         error_messages={
             'required': 'Password must not be empty'
         },
@@ -49,14 +48,14 @@ class RegisterForm(forms.ModelForm):
             'one lowercase letter and one number. The length should be '
             'at least 8 characters'
         ),
-        validators=[strong_password]
+        validators=[strong_password],
+        label='Password'
     )
 
     password2 = forms.CharField(
         required=True,
-        widget=forms.PasswordInput(attrs={
-            'placeholder': 'Repeat your password'
-        })
+        widget=forms.PasswordInput(),
+        label='Password2'
     )
 
     class Meta:
@@ -69,14 +68,11 @@ class RegisterForm(forms.ModelForm):
             'password',
         ]
 
-        # exclude = ['first_name']
-
         labels = {
             'username': 'Username',
             'first_name': 'First name',
             'last_name': 'Last name',
             'email': 'E-mail',
-            'passwprd': 'Password',
         }
         help_texts = {
             'email': 'The e-mail must be valid.',
@@ -86,40 +82,6 @@ class RegisterForm(forms.ModelForm):
                 'required': 'This field must not be empty',
             }
         }
-        widgets = {
-            'first_name': forms.TextInput(attrs={
-                'placeholder': 'Type your first name here',
-                'class': 'input text-input outra-classe'
-            }),
-            'password': forms.PasswordInput(attrs={
-                'placeholder': 'Type your password here.'
-            })
-
-        }
-
-    def clean_password(self):
-        data = self.cleaned_data.get('password')
-
-        if 'atenção' in data:
-            raise ValidationError(
-                'Não digite %(pipoca)s no campo password',
-                code='invalid',
-                params={'pipoca': '"atenção"'}
-            )
-
-        return data
-
-    def clean_first_name(self):
-        data = self.cleaned_data.get('first_name')
-
-        if 'John Doe' in data:
-            raise ValidationError(
-                'Não digite %(value)s no campo first name',
-                code='invalid',
-                params={'value': '"John Doe"'}
-            )
-
-        return data
 
     def clean(self):
         cleaned_data = super().clean()
